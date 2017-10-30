0e594a0ff82efbffdf58c35a62e9e3c3b865c7e9
JayloneZ <andrezyczkowski@gmail.com>
2017-10-30 13:12:21 +0000

    pw changed

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,15 +4,7 @@ class SqlRunner
 
   def self.run(sql, values)
     begin
-      db = PG.connect({
-#       dbname: 'burger_deals',
-#       host: 'localhost'
-        dbname: 'dc74ajbi6vlrjj',
-        host: 'ec2-23-23-221-255.compute-1.amazonaws.com',
-        port: '5432',
-        user: 'yndeqfvadjfzmj',
-        password: '9cee201532e8005cba42e22374e45d26edef006522234edbee0f5bafc9618e80',
-      })
+      db = PG.connect($db)
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
